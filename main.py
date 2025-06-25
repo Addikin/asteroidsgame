407b40f00210406ecea4040adfb4c43ce0a336d7
Addikin <addik3n@gmail.com>
2025-06-24 22:19:44 -0500

    Progress is definite

--- main.py
+++ main.py
@@ -2,26 +2,34 @@ import pygame
 from constants import *
 from player import *
 from circleshape import *
-
-print("Starting Asteroids!")
-print(f"Screen width: {SCREEN_WIDTH}")
-print(f"Screen height: {SCREEN_HEIGHT}")
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        dt = clock.tick(60) / 1000
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for drawing in drawable:
+            drawing.draw(screen)
         pygame.display.flip()
-    dt = clock.tick(60) / 1000
+    
 
 if __name__ == "__main__":
     main()
