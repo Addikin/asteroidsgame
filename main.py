807e36a3c1e228258e4fb4b58663f56f645e7056
Addikin <addik3n@gmail.com>
2025-06-26 22:41:01 -0500

    Adding shots and readability

--- main.py
+++ main.py
@@ -1,34 +1,57 @@
 import pygame
+import sys
 from constants import *
 from player import *
 from circleshape import *
 from asteroid import *
 from asteroidfield import *
+from shots import Shot
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+    
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     Asteroid.containers = (asteroids, updatable, drawable)
-    AsteroidField.containers = (updatable,)
-    Player.containers = (updatable, drawable)
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    Shot.containers = (shots, updatable, drawable)
+
+    AsteroidField.containers = updatable
     asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        dt = clock.tick(60) / 1000
+        
         updatable.update(dt)
+
+
+        for asteroid in asteroids:
+            if player.collision_check(asteroid):
+                print("Game over!")
+                sys.exit()            
         screen.fill("black")
+
+
         for drawing in drawable:
             drawing.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
     
 
 if __name__ == "__main__":
